7e9d117bff7a3e6f6c6982a0bf19a3092679515a
Jawoos <49528515+Jawoos@users.noreply.github.com>
2021-01-05 18:59:45 +0900

    add button menu

--- news_crwal.py
+++ news_crwal.py
@@ -9,6 +9,11 @@ import pymysql
 import signal
 import sys
 from datetime import datetime
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater
+from telegram.ext import CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+from telegram import ChatAction
+import os
 
 
 def save_log(log_msg):
@@ -37,131 +42,26 @@ def save_improve(improve_msg):
 
 
 def signal_handler(sig, frame):
-    save_log('SIGINT')
-    conn = pymysql.connect(host=db_ip, user=db_user, password=db_pw, database=db_name, port=db_port);
-    cursor = conn.cursor()
-    sql = "SELECT `usnum` FROM `user` WHERE `investKR_news` = 1 or 'naver_news' = 1"
-    cursor.execute(sql)
-    save_log(sql)
-    test = cursor.fetchall()
-    for tid in test:
-        bot.sendMessage(tid[0], "봇이 정지 되었습니다!!!\n금방 다시 실행되겠습니다~~!")
-    conn.close()
+    global count_err_msg
+    if pid != 0 and count_err_msg == 0:
+        count_err_msg = 1
+        save_log('SIGINT')
+        conn = pymysql.connect(host=db_ip, user=db_user, password=db_pw, database=db_name, port=db_port);
+        cursor = conn.cursor()
+        sql = "SELECT `usnum` FROM `user` WHERE `investKR_news` = 1 or 'naver_news' = 1"
+        cursor.execute(sql)
+        save_log(sql)
+        test = cursor.fetchall()
+        for tid in test:
+            bot_send.sendMessage(tid[0], "봇이 정지 되었습니다!!!\n금방 다시 실행되겠습니다~~!")
+        conn.close()
+        # os.kill(pid, signal.SIGINT)
     sys.exit(0)
 
 
-def Chatbot(msg):
-    global db_port, db_name, db_user, db_ip, db_pw
-    content_type, chat_type, chat_id, msg_date, msg_id = telepot.glance(msg, long=True)
-    conn = pymysql.connect(host=db_ip, user=db_user, password=db_pw, database=db_name, port=db_port);
-    cursor = conn.cursor()
-    sql = "SELECT * FROM `user` where usnum = " + str(chat_id) + ";"
-    cursor.execute(sql)
-    save_log(sql)
-    row = cursor.fetchall()
-    # print(msg)
-    if msg['text'] == "/Subscribe_investKR_News":    # invest.kr 뉴스 구독
-        if len(row) == 0:      # 데베에 등록되어 있지 않다면
-            cursor = conn.cursor()
-            sql = "INSERT INTO user(usnum, usname, investKR_news, naver_news) VALUES (" + str(chat_id) + \
-                  ", '" + msg['chat']['first_name'] + " " + msg['chat']['last_name'] + "', 1, 0);"
-            cursor.execute(sql)     # 데베에 유저 정보 등록
-            save_log(sql)
-            conn.commit()
-            bot.sendMessage(chat_id, "investing.kr 뉴스가 구독 되었습니다!")
-        elif len(row) != 0:     # 데베에 데이터가 있다면
-            cursor = conn.cursor()
-            sql = "SELECT `investKR_news` FROM `user` WHERE `usnum` = " + str(chat_id)
-            cursor.execute(sql)  # 데베에 등
-            save_log(sql)
-            test = cursor.fetchone()
-            if test[0] == 0:
-                cursor = conn.cursor()
-                sql = "UPDATE user SET `investKR_news` = 1 WHERE `usnum` = " + str(chat_id)
-                cursor.execute(sql)  # 데베에 가입 등록
-                save_log(sql)
-                conn.commit()
-                bot.sendMessage(chat_id, "investing.kr 뉴스가 구독 되었습니다!")
-            else:
-                bot.sendMessage(chat_id, "investing.kr 뉴스가 이미 구독중입니다!")
-
-    elif msg['text'] == "/Unsubscribe_investKR_News":      # invest.kr 뉴스 구독 해제
-        cursor = conn.cursor()
-        sql = "SELECT `investKR_news` FROM `user` WHERE `usnum` = " + str(chat_id)
-        cursor.execute(sql)  # 데베에 등
-        save_log(sql)
-        test = cursor.fetchone()
-        if test[0] == 0:
-            bot.sendMessage(chat_id, "investing.kr 뉴스가 구독중이 아니십니다!")
-            return
-        cursor = conn.cursor()
-        sql = "UPDATE user SET `investKR_news` = 0 WHERE `usnum` = " + str(chat_id)
-        cursor.execute(sql)  # 데베에 가입 등록
-        save_log(sql)
-        conn.commit()
-        bot.sendMessage(chat_id, "investing.kr 뉴스가 구독해제 되었습니다!")
-    elif msg['text'] == "/Subscribe_Naver_News":    # Naver 뉴스 구독
-        if len(row) == 0:      # 데베에 등록되어 있지 않다면
-            cursor = conn.cursor()
-            sql = "INSERT INTO user(usnum, usname, investKR_news, naver_news) VALUES (" + str(chat_id) + \
-                  ", '" + msg['chat']['first_name'] + " " + msg['chat']['last_name'] + "', 0, 1);"
-            cursor.execute(sql)     # 데베에 유저 정보 등록
-            save_log(sql)
-            conn.commit()
-            bot.sendMessage(chat_id, "네이버 뉴스가 구독 되었습니다!")
-        elif len(row) != 0:     # 데베에 데이터가 있다면
-            cursor = conn.cursor()
-            sql = "SELECT `naver_news` FROM `user` WHERE `usnum` = " + str(chat_id)
-            cursor.execute(sql)  # 데베에 등
-            save_log(sql)
-            test = cursor.fetchone()
-            if test[0] == 0:
-                cursor = conn.cursor()
-                sql = "UPDATE user SET `naver_news` = 1 WHERE `usnum` = " + str(chat_id)
-                cursor.execute(sql)  # 데베에 가입 등록
-                save_log(sql)
-                conn.commit()
-                bot.sendMessage(chat_id, "네이버 뉴스가 구독 되었습니다!")
-            else:
-                bot.sendMessage(chat_id, "네이버 뉴스가 이미 구독중입니다!")
-
-    elif msg['text'] == "/Unsubscribe_Naver_News":      # Naver 뉴스 구독 해제
-        cursor = conn.cursor()
-        sql = "SELECT `naver_news` FROM `user` WHERE `usnum` = " + str(chat_id)
-        cursor.execute(sql)  # 데베에 등
-        save_log(sql)
-        test = cursor.fetchone()
-        if test[0] == 0:
-            bot.sendMessage(chat_id, "네이버 뉴스가 구독중이 아니십니다!")
-            return
-        cursor = conn.cursor()
-        sql = "UPDATE user SET `naver_news` = 0 WHERE `usnum` = " + str(chat_id)
-        cursor.execute(sql)  # 데베에 가입 등록
-        save_log(sql)
-        conn.commit()
-        bot.sendMessage(chat_id, "네이버 뉴스가 구독해제 되었습니다!")
-    # elif msg['text'] == "/send_message_to_developer":      # 메세지 받기
-    #     print("get message")
-    #     MessageLoop(bot, save_improve)
-    elif msg['text'] == "/help":
-        bot.sendMessage(chat_id, help_str)
-    elif msg['text'] == "/start":
-        bot.sendMessage(chat_id, help_str)
-        if len(row) == 0:
-            cursor = conn.cursor()
-            sql = "INSERT INTO user(usnum, usname, investKR_news, naver_news) VALUES (" + str(chat_id) + \
-                  ", '" + msg['chat']['first_name'] + " " + msg['chat']['last_name'] + "', 0, 0);"
-            # print(sql)
-            cursor.execute(sql)  # 데베에 유저 정보 등록
-            save_log(sql)
-            conn.commit()
-    else:
-        save_improve(msg)
-    conn.close()
-
-
 def crawl_invest(str0):
-    global count_invest_err, admin_id, bot, count_popular, db_port, db_name, db_user, db_ip, db_pw
+    print('invest')
+    global count_invest_err, admin_id, bot_send, count_popular, db_port, db_name, db_user, db_ip, db_pw
     try:
         req = Request(str0)
         req.add_header('User-Agent', 'Mozilla/5.0')
@@ -190,7 +90,7 @@ def crawl_invest(str0):
                 save_log(sql)
                 test = cursor.fetchall()
                 for tid in test:
-                    bot.sendMessage(tid[0], msg)
+                    bot_send.sendMessage(tid[0], msg)
                 conn.close()
 
         time.sleep(60)
@@ -199,13 +99,14 @@ def crawl_invest(str0):
         print("error" + str(count_invest_err))
         if count_invest_err >= 500:
             print("err")
-            bot.sendMessage(admin_id, "도메인에 문제가 있습니다.")
+            bot_send.sendMessage(admin_id, "도메인에 문제가 있습니다.")
             count_invest_err = 0
         time.sleep(10)
 
 
 def crawl_naver():
-    global count_naver_err, admin_id, bot, count_naver, db_port, db_name, db_user, db_ip, db_pw
+    print('naver')
+    global count_naver_err, admin_id, bot_send, count_naver, db_port, db_name, db_user, db_ip, db_pw
     try:
         req = Request('https://finance.naver.com/news/news_list.nhn?mode=RANK')
         req.add_header('User-Agent', 'Mozilla/5.0')
@@ -213,6 +114,7 @@ def crawl_naver():
         soup = BeautifulSoup(html, "html.parser")
         soup = soup.find(class_="newsList")
         soup = soup.find_all('a')
+
         for row in soup:
             topic = row.text
             link = row.attrs['href']
@@ -233,7 +135,7 @@ def crawl_naver():
                 save_log(sql)
                 test = cursor.fetchall()
                 for tid in test:
-                    bot.sendMessage(tid[0], msg)
+                    bot_send.sendMessage(tid[0], msg)
                 conn.close()
         time.sleep(10)
     except:
@@ -241,9 +143,216 @@ def crawl_naver():
         print("error" + str(count_naver))
         if count_naver_err >= 500:
             print("err")
-            bot.sendMessage(admin_id, "도메인에 문제가 있습니다.")
+            bot_send.sendMessage(admin_id, "도메인에 문제가 있습니다.")
             count_naver_err = 0
         time.sleep(10)
+
+
+def start_command(update, context):
+    conn = pymysql.connect(host=db_ip, user=db_user, password=db_pw, database=db_name, port=db_port);
+    cursor = conn.cursor()
+    sql = "SELECT * FROM `user` where usnum = " + str(update.effective_user.id) + ";"
+    cursor.execute(sql)
+    save_log(sql)
+    row = cursor.fetchall()
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=help_str)
+    user = update.message.from_user
+    if len(row) == 0:
+        cursor = conn.cursor()
+        sql = "INSERT INTO user(usnum, usname, investKR_news, naver_news) VALUES (" \
+              + str(update.effective_chat.id) + \
+              ", '" + user['first_name'] + " " + user['last_name'] + "', 0, 0);"
+        cursor.execute(sql)  # 데베에 유저 정보 등록
+        save_log(sql)
+        conn.commit()
+    conn.close()
+
+
+def cmd_task_buttons(update, context):
+    task_buttons = [[InlineKeyboardButton('국내 뉴스 구독', callback_data=1),
+                     InlineKeyboardButton('국내 뉴스 구독 해제', callback_data=2)],
+                    [InlineKeyboardButton('국제 뉴스 구독', callback_data=3),
+                    InlineKeyboardButton('국제 뉴스 구독 해제', callback_data=4)],
+                    [InlineKeyboardButton('개발자에게 건의사항', callback_data=7)]]
+    reply_markup = InlineKeyboardMarkup(task_buttons)
+    context.bot.send_message(
+        chat_id=update.message.chat_id,
+        text='아래 작업중 하나를 선택해 주세요!!',
+        reply_markup=reply_markup
+    )
+
+
+def cb_button(update, context):
+    global improve_msg, updater, dispatcher
+    # database setting
+    conn = pymysql.connect(host=db_ip, user=db_user, password=db_pw, database=db_name, port=db_port);
+    cursor = conn.cursor()
+    sql = "SELECT * FROM `user` where usnum = " + str(update.effective_user.id) + ";"
+    cursor.execute(sql)
+    save_log(sql)
+    row = cursor.fetchall()
+
+    query = update.callback_query
+    data = query.data
+    context.bot.send_chat_action(
+        chat_id=update.effective_user.id,
+        action=ChatAction.TYPING
+    )
+    if data == '1':     # 네이버 뉴스 구독
+        if len(row) == 0:  # 데베에 등록되어 있지 않다면
+            cursor = conn.cursor()
+            sql = "INSERT INTO user(usnum, usname, investKR_news, naver_news) VALUES (" \
+                  + str(update.effective_user.id) + \
+                  ", '" + query['chat']['first_name'] + " " + query['chat']['last_name'] + "', 0, 1);"
+            print(sql)
+            cursor.execute(sql)  # 데베에 유저 정보 등록
+            save_log(sql)
+            conn.commit()
+            context.bot.edit_message_text(
+                text='네이버 뉴스가 구독 되었습니다!',
+                chat_id=query.message.chat_id,
+                message_id=query.message.message_id
+            )
+        elif len(row) != 0:  # 데베에 데이터가 있다면
+            cursor = conn.cursor()
+            sql = "SELECT `naver_news` FROM `user` WHERE `usnum` = " + str(update.effective_user.id)
+            cursor.execute(sql)  # 데베에 등
+            save_log(sql)
+            test = cursor.fetchone()
+            if test[0] == 0:
+                cursor = conn.cursor()
+                sql = "UPDATE user SET `naver_news` = 1 WHERE `usnum` = " + str(update.effective_user.id)
+                cursor.execute(sql)  # 데베에 가입 등록
+                save_log(sql)
+                conn.commit()
+                context.bot.edit_message_text(
+                    text='네이버 뉴스가 구독 되었습니다!',
+                    chat_id=query.message.chat_id,
+                    message_id=query.message.message_id
+                )
+            else:
+                context.bot.edit_message_text(
+                    text='네이버 뉴스가 이미 구독중입니다!',
+                    chat_id=query.message.chat_id,
+                    message_id=query.message.message_id
+                )
+    if data == '2':     # 네이버 뉴스 구독 해제
+        cursor = conn.cursor()
+        sql = "SELECT `naver_news` FROM `user` WHERE `usnum` = " + str(update.effective_user.id)
+        cursor.execute(sql)  # 데베에 등
+        save_log(sql)
+        test = cursor.fetchone()
+        if test[0] == 0:
+            context.bot.edit_message_text(
+                text='네이버 뉴스를  구독중이 아니십니다!',
+                chat_id=query.message.chat_id,
+                message_id=query.message.message_id
+            )
+            return
+        cursor = conn.cursor()
+        sql = "UPDATE user SET `naver_news` = 0 WHERE `usnum` = " + str(update.effective_user.id)
+        cursor.execute(sql)  # 데베에 가입 등록
+        save_log(sql)
+        conn.commit()
+        context.bot.edit_message_text(
+            text='네이버 뉴스가 구독해제 되었습니다!',
+            chat_id=query.message.chat_id,
+            message_id=query.message.message_id
+        )
+    if data == '3':     # investing 뉴스 구독
+        if len(row) == 0:  # 데베에 등록되어 있지 않다면
+            cursor = conn.cursor()
+            sql = "INSERT INTO user(usnum, usname, investKR_news, naver_news) VALUES (" \
+                  + str(update.effective_user.id) + \
+                  ", '" + query['chat']['first_name'] + " " + query['chat']['last_name'] + "', 1, 0);"
+            cursor.execute(sql)  # 데베에 유저 정보 등록
+            save_log(sql)
+            conn.commit()
+            context.bot.edit_message_text(
+                text='investing.kr 뉴스가 구독 되었습니다!',
+                chat_id=query.message.chat_id,
+                message_id=query.message.message_id
+            )
+        elif len(row) != 0:  # 데베에 데이터가 있다면
+            cursor = conn.cursor()
+            sql = "SELECT `investKR_news` FROM `user` WHERE `usnum` = " + str(update.effective_user.id)
+            cursor.execute(sql)  # 데베에 등
+            save_log(sql)
+            test = cursor.fetchone()
+            if test[0] == 0:
+                cursor = conn.cursor()
+                sql = "UPDATE user SET `investKR_news` = 1 WHERE `usnum` = " + str(update.effective_user.id)
+                cursor.execute(sql)  # 데베에 가입 등록
+                save_log(sql)
+                conn.commit()
+                context.bot.edit_message_text(
+                    text='investing.kr 뉴스가 구독 되었습니다!',
+                    chat_id=query.message.chat_id,
+                    message_id=query.message.message_id
+                )
+            else:
+                context.bot.edit_message_text(
+                    text='investing.kr 뉴스가 이미 구독중입니다!',
+                    chat_id=query.message.chat_id,
+                    message_id=query.message.message_id
+                )
+    if data == '4':     # investing 뉴스 구독 해제
+        cursor = conn.cursor()
+        sql = "SELECT `investKR_news` FROM `user` WHERE `usnum` = " + str(update.effective_user.id)
+        cursor.execute(sql)  # 데베에 등
+        save_log(sql)
+        test = cursor.fetchone()
+        if test[0] == 0:
+            context.bot.edit_message_text(
+                text='investing.kr 뉴스가 구독중이 아니십니다!',
+                chat_id=query.message.chat_id,
+                message_id=query.message.message_id
+            )
+            return
+        cursor = conn.cursor()
+        sql = "UPDATE user SET `investKR_news` = 0 WHERE `usnum` = " + str(update.effective_user.id)
+        cursor.execute(sql)  # 데베에 가입 등록
+        save_log(sql)
+        conn.commit()
+        context.bot.edit_message_text(
+            text='investing.kr 뉴스가 구독해제 되었습니다!',
+            chat_id=query.message.chat_id,
+            message_id=query.message.message_id
+        )
+    if data == '7':
+        message_handler = MessageHandler(Filters.text, get_message)
+        dispatcher.add_handler(message_handler)
+        while improve_msg == "":
+            continue
+        print(improve_msg)
+        improve_msg = ""
+        dispatcher.remove_handler(message_handler)
+        # bot = telepot.Bot(test_token)
+        # resp = bot.update
+        # print(resp)
+
+        # print(update.message)
+        # chat = telegram.Bot(token=test_token)
+        # updates = chat.getUpdates()
+        # print(updates)
+        # for u in updates:
+        #     print(u.message)
+
+
+    # context.bot.edit_message_text(
+    #     text='[{}] 작업을 완료하였습니다.'.format(data),
+    #     chat_id=query.message.chat_id,
+    #     message_id=query.message.message_id
+    # )
+    conn.close()
+
+
+def get_message(update, context):
+    global improve_msg
+    print(update)
+    improve_msg = update
+
 
 
 # main
@@ -266,7 +375,10 @@ token_file = open("token_data.txt", "r", encoding="UTF8")
 id_file = open("id_data.txt", "r", encoding="UTF8")
 db_file = open("db_data.txt", "r", encoding="UTF8")
 my_token = token_file.readline()
-my_token = my_token[1:]
+my_token = my_token[1:len(my_token) - 1]
+test_token = token_file.readline()
+test_token = test_token[:len(test_token) - 1]
+alarm_token = token_file.readline()
 admin_id = id_file.readline()
 db_ip = db_file.readline()
 db_ip = db_ip[:len(db_ip)-1]
@@ -284,7 +396,9 @@ db_file.close()
 
 tele_id = []
 
-for_the_first = 0;
+improve_msg = ""
+for_the_first = 0
+count_err_msg = 0
 count_naver = 0
 count_popular = 0
 count_invest_err = 0    # invest error 횟수
@@ -293,27 +407,49 @@ queue_popular = []
 queue_naver = []
 
 signal.signal(signal.SIGINT, signal_handler)
-bot = telepot.Bot(my_token)
-MessageLoop(bot, Chatbot).run_as_thread()
+
+
+# bot = telepot.Bot(my_token)
+bot_send = telepot.Bot(test_token)     # for test
+# MessageLoop(bot, Chatbot).run_as_thread()
+
+
+updater = Updater(token=test_token, use_context=True)
+dispatcher = updater.dispatcher
+
+start_handler = CommandHandler('start', start_command)
+task_buttons_handler = CommandHandler('tasks', cmd_task_buttons)
+button_callback_handler = CallbackQueryHandler(cb_button)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(task_buttons_handler)
+dispatcher.add_handler(button_callback_handler)
+
+pid = os.fork()
+if pid == 0:
+    updater.start_polling()
+    updater.idle()
+
+
 conn = pymysql.connect(host=db_ip, user=db_user, password=db_pw, database=db_name, port=db_port);
 cursor = conn.cursor()
 sql = "SELECT `usnum` FROM `user` WHERE `investKR_news` = 1 or 'naver_news' = 1"
 cursor.execute(sql)
 save_log(sql)
 test = cursor.fetchall()
-for tid in test:
-    try:
-        bot.sendMessage(tid[0], "봇이 다시 실행 되었습니다!\n"
-                                "유익한 뉴스를 다시 제공해 드리겠습니다~~!!\n"
-                                "사용법이 궁금하시다면 /help 를 입력해주세요!!")
-    except telepot.exception.BotWasBlockedError:
-        print("err", end = ' ')
-        print(tid[0])
-        sql_temp = "DELETE FROM `user` WHERE `usnum` = " + str(tid[0])
-        cursor.execute(sql_temp)
-        save_log(sql)
-        conn.commit()
-        time.sleep(100)
+# for tid in test:
+#     try:
+#         bot.sendMessage(tid[0], "봇이 다시 실행 되었습니다!\n"
+#                                 "유익한 뉴스를 다시 제공해 드리겠습니다~~!!\n"
+#                                 "사용법이 궁금하시다면 /help 를 입력해주세요!!")
+#     except telepot.exception.BotWasBlockedError:
+#         print("err", end=' ')
+#         print(tid[0])
+#         sql_temp = "DELETE FROM `user` WHERE `usnum` = " + str(tid[0])
+#         cursor.execute(sql_temp)
+#         save_log(sql)
+#         conn.commit()
+#         time.sleep(100)
 conn.close()
 while True:     # 뉴스 크롤링 파트
     crawl_invest(str_url)
